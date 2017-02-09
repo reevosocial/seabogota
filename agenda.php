f0b2343596fd8e807f22da0cb94c73ebaf3378de
German <german@reevo.org>
2017-02-09 16:53:15 -0300

    aisla el código del footer

--- agenda.php
+++ agenda.php
@@ -1429,28 +1429,4 @@ include("menu.php");
 
 		</div>
 </div>
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Carousel -->
-	<script src="js/owl.carousel.min.js"></script>
-	<!-- Stellar -->
-	<script src="js/jquery.stellar.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Counters -->
-	<script src="js/jquery.countTo.js"></script>
-	
-	
-	<!-- MAIN JS -->
-	<script src="js/main.js"></script>
-
-
-
-
-	</body>
-</html>
-
+		<?php include("footer.php");?>
--- videos.php
+++ videos.php
@@ -164,38 +164,4 @@ include("menu.php");
 					
 	</div>
 </div>
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Carousel -->
-	<script src="js/owl.carousel.min.js"></script>
-	<!-- Stellar -->
-	<script src="js/jquery.stellar.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Counters -->
-	<script src="js/jquery.countTo.js"></script>
-	
-	
-	<!-- MAIN JS -->
-	<script src="js/main.js"></script>
-<script type="text/javascript">
-
-$(function () {
-    var viewer = ImageViewer();
-    $('.gallery-items').click(function () {
-        var imgSrc = $(this).data('low-src'),
-            highResolutionImage = $(this).data('high-res-src');
-        viewer.show(imgSrc, highResolutionImage);
-    });
-});
-</script>
-
-
-
-	</body>
-</html>
-
+		<?php include("footer.php");?>
